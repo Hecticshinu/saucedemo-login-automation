e75acd151315b4584d8a9f7db3e3974e60fe4218
Shinu <varunsingh000999@gmail.com>
2026-09-22 00:59:32 +0530

    Remove blank login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -25,21 +25,6 @@ def test_valid_login(driver):
     time.sleep(2)
 
 
-def test_blank_login(driver):
-
-    page = LoginPage(driver)
-
-    page.open()
-    time.sleep(2)
-
-    page.click_login()
-    time.sleep(2)
-
-    assert "Username is required" in page.get_error_message()
-
-    time.sleep(2)
-
-
 def test_valid_login_and_logout(driver):
 
     page = LoginPage(driver)
