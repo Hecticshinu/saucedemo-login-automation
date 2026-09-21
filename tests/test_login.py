6f6299756c1609c5dd0acfb4c3fe9385f2d81b4a
Shinu <varunsingh000999@gmail.com>
2026-09-22 00:42:27 +0530

    Remove invalid password test

--- tests/test_login.py
+++ tests/test_login.py
@@ -61,20 +61,3 @@ def test_valid_login_and_logout(driver):
     assert driver.current_url == "https://www.saucedemo.com/"
 
     time.sleep(2)
-   
-def test_invalid_password(driver):
-
-    page = LoginPage(driver)
-
-    page.open()
-    time.sleep(2)
-
-    page.login(
-        VALID_USERNAME,
-        "wrong_password"
-    )
-    time.sleep(2)
-
-    assert "Username and password do not match" in page.get_error_message()
-
-    time.sleep(2)
